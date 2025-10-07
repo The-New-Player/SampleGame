afbf5d5b78d658d5e1dbc6bf3faabd39270c9bf1
The-new-player <wasasa0222@gmail.com>
2025-10-07 11:52:10 +0900

    add Create & Enter

--- public/sketch.js
+++ public/sketch.js
@@ -56,76 +56,195 @@
 
 
 
-let socket;
-let board = Array(3).fill().map(() => Array(3).fill(null));
-let turn = "O";
-let myTurn = false;
-let roomId;
 
-let clearButton;
+
+// let socket;
+// let board = Array(3).fill().map(() => Array(3).fill(null));
+// let turn = "O";
+// let myTurn = false;
+// let roomId;
+
+// let clearButton;
+
+// function setup() {
+//   createCanvas(400, 400);
+//   clearButton = createButton("クリア");
+//   clearButton.position(10, 420);
+//   clearButton.mousePressed(() => {
+//     socket.emit("clear", roomId);
+//   });
+
+//   socket = io();
+
+//   // URLからroomIdを取得
+//   const pathParts = window.location.pathname.split("/");
+//   roomId = pathParts[pathParts.length - 1];
+//   socket.emit("joinRoom", roomId);
+
+//   socket.on("startGame", () => {
+//     myTurn = true;
+//     console.log("Game started!");
+//   });
+
+//   socket.on("move", (data) => {
+//     board[data.i][data.j] = data.player;
+//     turn = data.player === "O" ? "X" : "O";
+//   });
+
+//   socket.on("clear", () => {
+//     clearBoard();
+//   });
+// }
+
+// function draw() {
+//   background(255);
+//   stroke(0);
+//   for (let i = 1; i < 3; i++) {
+//     line(i * 100, 0, i * 100, 300);
+//     line(0, i * 100, 300, i * 100);
+//   }
+
+//   textSize(64);
+//   textAlign(CENTER, CENTER);
+//   for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < 3; j++) {
+//       if (board[i][j]) text(board[i][j], j * 100 + 50, i * 100 + 50);
+//     }
+//   }
+// }
+
+// function mousePressed() {
+//   if (!myTurn) return; // 順番じゃなければ無視
+
+//   let i = floor(mouseY / 100);
+//   let j = floor(mouseX / 100);
+//   if (!board[i][j]) {
+//     board[i][j] = turn;
+//     socket.emit("move", { i, j, player: turn, roomId });
+//     turn = turn === "O" ? "X" : "O";
+//     myTurn = false; // ターン交代
+//   }
+// }
+
+// function clearBoard() {
+//   board = Array(3).fill().map(() => Array(3).fill(null));
+//   turn = "O";
+//   myTurn = true;
+// }
+
+
+
+
+let socket;
+let roomId;
+let myTurn = false;
+let mySymbol = null;
+let board = Array(9).fill(null);
+let statusMessage = "Connecting...";
 
 function setup() {
-  createCanvas(400, 400);
-  clearButton = createButton("クリア");
-  clearButton.position(10, 420);
-  clearButton.mousePressed(() => {
-    socket.emit("clear", roomId);
-  });
-
+  createCanvas(400, 450);
   socket = io();
 
-  // URLからroomIdを取得
-  const pathParts = window.location.pathname.split("/");
-  roomId = pathParts[pathParts.length - 1];
-  socket.emit("joinRoom", roomId);
+  const path = window.location.pathname;
+  if (path === "/") setupHome();
+  else if (path === "/create") setupCreate();
+  else if (path === "/enter") setupEnter();
+  else if (path.startsWith("/room/")) {
+    roomId = path.split("/")[2];
+    setupRoom();
+  }
+}
 
-  socket.on("startGame", () => {
-    myTurn = true;
-    console.log("Game started!");
-  });
+function setupHome() {
+  createButton("Create").mousePressed(() => (window.location.href = "/create"));
+  createButton("Enter").mousePressed(() => (window.location.href = "/enter"));
+}
 
-  socket.on("move", (data) => {
-    board[data.i][data.j] = data.player;
-    turn = data.player === "O" ? "X" : "O";
-  });
-
-  socket.on("clear", () => {
-    clearBoard();
+function setupCreate() {
+  const code = nf(floor(random(0, 999999)), 6);
+  createP(`Room Code: ${code}`);
+  createButton("Create Room").mousePressed(() => {
+    window.location.href = `/room/${code}`;
   });
 }
 
+function setupEnter() {
+  const input = createInput("");
+  input.attribute("placeholder", "Enter 6-digit code");
+  createButton("Enter Room").mousePressed(() => {
+    const code = input.value();
+    if (code.match(/^\d{6}$/)) {
+      window.location.href = `/room/${code}`;
+    } else {
+      alert("Please enter a 6-digit number");
+    }
+  });
+}
+
+function setupRoom() {
+  socket.emit("joinRoom", roomId);
+
+  // 自分のシンボルを受け取る
+  socket.on("assignSymbol", (symbol) => {
+    mySymbol = symbol;
+    myTurn = symbol === "O"; // Oが先手
+    statusMessage = `You are ${mySymbol}. Waiting for another player...`;
+  });
+
+  // 2人揃ったらゲーム開始
+  socket.on("startGame", () => {
+    statusMessage = `Game start! ${mySymbol === "O" ? "Your turn" : "Opponent's turn"}`;
+  });
+
+  // 相手が動いたら更新
+  socket.on("move", (data) => {
+    board[data.index] = data.symbol;
+    myTurn = data.symbol !== mySymbol; // 相手のターンが終わったら自分のターン
+  });
+
+  socket.on("roomFull", () => {
+    statusMessage = "This room is full.";
+  });
+}
+
+function mousePressed() {
+  if (!myTurn || !mySymbol) return;
+  if (mouseY > 400) return; // ボード外
+
+  const i = floor(mouseX / 133);
+  const j = floor(mouseY / 133);
+  const index = i + j * 3;
+
+  if (index >= 0 && index < 9 && !board[index]) {
+    board[index] = mySymbol;
+    socket.emit("move", { roomId, index, symbol: mySymbol });
+    myTurn = false;
+    statusMessage = "Waiting for opponent...";
+  }
+}
+
 function draw() {
-  background(255);
-  stroke(0);
+  background(240);
+  drawBoard();
+  textSize(16);
+  textAlign(CENTER);
+  fill(0);
+  text(statusMessage, width / 2, 430);
+}
+
+function drawBoard() {
+  strokeWeight(2);
   for (let i = 1; i < 3; i++) {
-    line(i * 100, 0, i * 100, 300);
-    line(0, i * 100, 300, i * 100);
+    line(i * 133, 0, i * 133, 400);
+    line(0, i * 133, 400, i * 133);
   }
 
   textSize(64);
   textAlign(CENTER, CENTER);
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (board[i][j]) text(board[i][j], j * 100 + 50, i * 100 + 50);
-    }
+  for (let i = 0; i < 9; i++) {
+    const x = (i % 3) * 133 + 66;
+    const y = floor(i / 3) * 133 + 66;
+    text(board[i] || "", x, y);
   }
-}
-
-function mousePressed() {
-  if (!myTurn) return; // 順番じゃなければ無視
-
-  let i = floor(mouseY / 100);
-  let j = floor(mouseX / 100);
-  if (!board[i][j]) {
-    board[i][j] = turn;
-    socket.emit("move", { i, j, player: turn, roomId });
-    turn = turn === "O" ? "X" : "O";
-    myTurn = false; // ターン交代
-  }
-}
-
-function clearBoard() {
-  board = Array(3).fill().map(() => Array(3).fill(null));
-  turn = "O";
-  myTurn = true;
 }
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-import { randomBytes } from "crypto";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
@@ -9,49 +13,66 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-// ルーム作成用
+// ====== ページ ======
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
 app.get("/create", (req, res) => {
-  const roomId = randomBytes(3).toString("hex"); // 6桁の部屋ID
-  res.redirect(`/room/${roomId}`);
+  res.sendFile(path.join(__dirname, "public/create.html"));
 });
 
-// 通常ページ
+app.get("/enter", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/enter.html"));
+});
+
 app.get("/room/:roomId", (req, res) => {
-  res.sendFile(process.cwd() + "/public/index.html");
+  res.sendFile(path.join(__dirname, "public/room.html"));
 });
 
+// ====== Socket.IO ======
 const rooms = {}; // { roomId: [socketId1, socketId2] }
 
 io.on("connection", (socket) => {
   console.log("connected:", socket.id);
 
   socket.on("joinRoom", (roomId) => {
-    socket.join(roomId);
     if (!rooms[roomId]) rooms[roomId] = [];
+
+    // 部屋が満員なら入れない
+    if (rooms[roomId].length >= 2) {
+      socket.emit("roomFull");
+      return;
+    }
+
+    socket.join(roomId);
     rooms[roomId].push(socket.id);
 
-    console.log(`User ${socket.id} joined room ${roomId}`);
+    // O / X を割り当てる
+    const playerSymbol = rooms[roomId].length === 1 ? "O" : "X";
+    socket.emit("assignSymbol", playerSymbol);
+
+    console.log(`User ${socket.id} joined room ${roomId} as ${playerSymbol}`);
     console.log("Current rooms:", rooms);
 
-    // 2人そろったら開始信号
+    // 2人揃ったらゲーム開始
     if (rooms[roomId].length === 2) {
       io.to(roomId).emit("startGame");
     }
   });
 
-  // プレイヤーの手をルーム内に送信
   socket.on("move", (data) => {
+    // data = { roomId, x, y, symbol }
     io.to(data.roomId).emit("move", data);
   });
 
-  // クリアボタンもルーム内だけに反映
   socket.on("clear", (roomId) => {
     io.to(roomId).emit("clear");
   });
 
   socket.on("disconnect", () => {
     for (const roomId in rooms) {
-      rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+      rooms[roomId] = rooms[roomId].filter((id) => id !== socket.id);
       if (rooms[roomId].length === 0) delete rooms[roomId];
     }
     console.log("disconnected:", socket.id);
@@ -60,6 +81,75 @@ io.on("connection", (socket) => {
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+
+
+
+// import express from "express";
+// import http from "http";
+// import { Server } from "socket.io";
+// import { randomBytes } from "crypto";
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server);
+
+// app.use(express.static("public"));
+
+// // ルーム作成用
+// app.get("/create", (req, res) => {
+//   const roomId = randomBytes(3).toString("hex"); // 6桁の部屋ID
+//   res.redirect(`/room/${roomId}`);
+// });
+
+// // 通常ページ
+// app.get("/room/:roomId", (req, res) => {
+//   res.sendFile(process.cwd() + "/public/index.html");
+// });
+
+// const rooms = {}; // { roomId: [socketId1, socketId2] }
+
+// io.on("connection", (socket) => {
+//   console.log("connected:", socket.id);
+
+//   socket.on("joinRoom", (roomId) => {
+//     socket.join(roomId);
+//     if (!rooms[roomId]) rooms[roomId] = [];
+//     rooms[roomId].push(socket.id);
+
+//     console.log(`User ${socket.id} joined room ${roomId}`);
+//     console.log("Current rooms:", rooms);
+
+//     // 2人そろったら開始信号
+//     if (rooms[roomId].length === 2) {
+//       io.to(roomId).emit("startGame");
+//     }
+//   });
+
+//   // プレイヤーの手をルーム内に送信
+//   socket.on("move", (data) => {
+//     io.to(data.roomId).emit("move", data);
+//   });
+
+//   // クリアボタンもルーム内だけに反映
+//   socket.on("clear", (roomId) => {
+//     io.to(roomId).emit("clear");
+//   });
+
+//   socket.on("disconnect", () => {
+//     for (const roomId in rooms) {
+//       rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+//       if (rooms[roomId].length === 0) delete rooms[roomId];
+//     }
+//     console.log("disconnected:", socket.id);
+//   });
+// });
+
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => console.log(`Server running on port ${port}`));
+
+
 
 
 
